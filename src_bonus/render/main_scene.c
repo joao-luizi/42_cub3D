73de1dea15dfd0cd903844052c3f8c3ee371b611
joao luizi <joao.luizi@gmail.com>
2025-05-18 23:06:19 +0100

    ceiling and floor render

--- src_bonus/render/main_scene.c
+++ src_bonus/render/main_scene.c
@@ -6,7 +6,7 @@
 /*   By: joaomigu <joaomigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 15:25:44 by joaomigu          #+#    #+#             */
-/*   Updated: 2025/05/18 19:13:28 by joaomigu         ###   ########.fr       */
+/*   Updated: 2025/05/18 23:05:57 by joaomigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,6 +92,48 @@ static inline void initialize_ray(t_app_state *st, t_ray_info *r_info, int x)
     r_info->wall = NONE_WALL
 }
 
+static void render_ceiling_and_floor(t_app_state *st, int x)
+{
+    int y;
+    double row_distance;
+    double floor_x, floor_y;
+    double weight;
+    int tex_x, tex_y;
+
+    t_img *floor_texture = st->textures.floor;
+    t_img *ceiling_texture = st->textures.ceiling;
+
+    for (y = MAIN_HEIGHT / 2; y < MAIN_HEIGHT; y++) // Floor
+    {
+        // Calculate the distance from the player to the floor plane
+        row_distance = (0.5 * MAIN_HEIGHT) / (y - MAIN_HEIGHT / 2);
+
+        // Calculate the real-world coordinates of the floor point
+        weight = row_distance / st->player.plane_dist;
+        floor_x = st->player.position.x + weight * (st->player.dir.x + st->player.plane.x * (2 * x / (double)MAIN_WIDTH - 1));
+        floor_y = st->player.position.y + weight * (st->player.dir.y + st->player.plane.y * (2 * x / (double)MAIN_WIDTH - 1));
+
+        // Map the world coordinates to texture coordinates
+        tex_x = (int)(floor_x * floor_texture->width) % floor_texture->width;
+        tex_y = (int)(floor_y * floor_texture->height) % floor_texture->height;
+
+        // Fetch the pixel from the floor texture
+        int floor_color = get_texture_pixel(floor_texture, tex_x, tex_y);
+
+        // Draw the floor pixel
+        put_pixel_to_image(st->image, x, y, floor_color);
+
+        // For the ceiling, invert the y-coordinate
+        int ceiling_y = MAIN_HEIGHT - y - 1;
+
+        // Fetch the pixel from the ceiling texture
+        int ceiling_color = get_texture_pixel(ceiling_texture, tex_x, tex_y);
+
+        // Draw the ceiling pixel
+        put_pixel_to_image(st->image, x, ceiling_y, ceiling_color);
+    }
+}
+
 void *raycast_routine(void *arg)
 {
     t_args      *args;
@@ -106,6 +148,11 @@ void *raycast_routine(void *arg)
     {
         if (!pre_wait(st))
             break;
+            
+        for (x = args->start_col; x < args->end_col; x++)
+        {
+            render_ceiling_and_floor(st, x);
+        }
         x = args->start_col - 1;
         while (++x < args->end_col)
         {
