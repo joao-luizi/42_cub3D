b30056e42b2de8ac7446bdbf0caf57782e5fe63b
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2025-06-03 11:33:23 +0100

    Add norm fix for render/post_process.c

--- src_bonus/render/post_process.c
+++ src_bonus/render/post_process.c
@@ -12,6 +12,24 @@
 
 #include "../../inc_bonus/cub3d.h"
 
+static void	print_fps_util(t_app_state *st, int fps)
+{
+	char	*itoa;
+
+	itoa = ft_itoa(fps);
+	if (!itoa)
+		return ;
+	lib_x_write_string("FPS", FPS_X + 10, FPS_Y + 20, &st->g.main_scene);
+	lib_x_write_string(itoa, FPS_X + 80, FPS_Y + 20, &st->g.main_scene);
+	free(itoa);
+	itoa = ft_itoa(st->core_count);
+	if (!itoa)
+		return ;
+	lib_x_write_string("CORES", FPS_X + 10, FPS_Y, &st->g.main_scene);
+	lib_x_write_string(itoa, FPS_X + 80, FPS_Y, &st->g.main_scene);
+	free(itoa);
+}
+
 /**
  * @brief Calculates and displays the current frames per second (FPS) on
  * the screen.
@@ -27,7 +45,6 @@ void	print_fps(t_app_state *st)
 	struct timeval	current;
 	int				frame_time;
 	int				fps;
-	char			*itoa;
 
 	gettimeofday(&current, NULL);
 	if (st->previous_time.tv_sec != 0 || st->previous_time.tv_usec != 0)
@@ -44,17 +61,11 @@ void	print_fps(t_app_state *st)
 		frame_time = 0;
 		fps = 0;
 	}
-	itoa = ft_itoa(fps);
-	lib_x_write_string("FPS", FPS_X + 10, FPS_Y + 20, &st->g.main_scene);
-	lib_x_write_string(itoa, FPS_X + 80, FPS_Y + 20, &st->g.main_scene);
-	free(itoa);
-	itoa = ft_itoa(st->core_count);
-	lib_x_write_string("CORES", FPS_X + 10, FPS_Y, &st->g.main_scene);
-	lib_x_write_string(itoa, FPS_X + 80, FPS_Y, &st->g.main_scene);
-	free(itoa);
+	print_fps_util(st, fps);
 }
 
-static inline void	blit_face_frame(t_img *dst, t_img *src, t_point p, t_args *args)
+static inline void	blit_face_frame(t_img *dst, t_img *src,
+									t_point p, t_args *args)
 {
 	int	color;
 	int	x;
@@ -77,22 +88,28 @@ static inline void	blit_face_frame(t_img *dst, t_img *src, t_point p, t_args *ar
 		}
 	}
 }
-static bool belongs_in_slice(t_args *args, int x)
+
+static bool	belongs_in_slice(t_args *args, int x)
 {
 	return (x >= args->start_col && x < args->end_col);
 }
+
 void	post_process(t_app_state *st, t_args *args)
 {
-	int frame;
-	t_img *face_frame;
+	int		frame;
+	t_img	*face_frame;
 
 	frame = st->anims[0].current_frame;
 	face_frame = &st->g.face_anim[frame].frame;
-	if (st->g.face && (belongs_in_slice(args, FACE_X) || belongs_in_slice(args, FACE_X + face_frame->width)))
-		blit_face_frame(&st->g.main_scene, face_frame, (t_point){ FACE_X, MAIN_HEIGHT
-			- (FACE_Y + face_frame->height) }, args);
-	if (st->g.fps && (belongs_in_slice(args, FPS_X) || belongs_in_slice(args, FPS_X + face_frame->width)))
+	if (st->g.face && (belongs_in_slice(args, FACE_X)
+			|| belongs_in_slice(args, FACE_X + face_frame->width)))
+		blit_face_frame(&st->g.main_scene,
+			face_frame, (t_point){FACE_X, MAIN_HEIGHT
+			- (FACE_Y + face_frame->height)}, args);
+	if (st->g.fps && (belongs_in_slice(args, FPS_X)
+			|| belongs_in_slice(args, FPS_X + face_frame->width)))
 		print_fps(st);
-	if (st->g.minimap && (belongs_in_slice(args, MINIMAP_X) || belongs_in_slice(args, MINIMAP_X + MINIMAP_WIDTH)))
+	if (st->g.minimap && (belongs_in_slice(args, MINIMAP_X)
+			|| belongs_in_slice(args, MINIMAP_X + MINIMAP_WIDTH)))
 		render_minimap(st, args);
 }
